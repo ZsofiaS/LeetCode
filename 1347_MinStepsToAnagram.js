25c7bd59ee7417ea5a076a0d86d0771d455e4d7e
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-10-12 11:36:38 +0100

    1347. Minimum Number of Steps to Make Two Strings Anagram - more efficient solution

--- 1347_MinStepsToAnagram.js
+++ 1347_MinStepsToAnagram.js
@@ -1,15 +1,19 @@
 // 1347. Minimum Number of Steps to Make Two Strings Anagram
 // https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram/
 
+// more efficient solution
 let minSteps = function(s, t) {
-    let output = '';
-    for (let i = 0; i < s.length; i++) {
-        for (let j = 0; j < t.length; j++) {
-            if (s[i] === t[j]) {
-                t = t.replace(t[j], '');
-                break;
-            }
-        } 
+    let map = {};
+    let counter = 0;
+    for (let letter in s) {
+        map[letter] = map[letter] + 1 || 1;
     }
-    return t.length;
+    for (let letter in t) {
+        if (map[letter]) {
+            map[letter]--;
+            continue;
+        }
+        counter++;
+    }
+    return counter;
 };
