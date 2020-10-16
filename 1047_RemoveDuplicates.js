f45db8e6d2430a60b54e1c48df10a18e81699547
Zsofia Szonyi <szonyi.zsofia@gmail.com>
2020-10-16 21:59:55 +0100

    1047. Remove All Adjacent Duplicates In String - amended

--- 1047_RemoveDuplicates.js
+++ 1047_RemoveDuplicates.js
@@ -5,11 +5,9 @@ let removeDuplicates = function(S) {
     let array = S.split('');
     for (let i = 0; i < array.length - 1; i++) {
         if (array[i] === array[i+1]) {
-            array.splice(i+1, 1);
-            array.splice(i, 1);
-            S = array.join('');
+            array.splice(i, 2);
             i = -1;
         }   
     }
-    return S;
+    return array.join('');
 };
